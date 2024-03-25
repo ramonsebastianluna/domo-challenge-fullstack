0769af90b0a0dc0312f4091482bcdd9a132f474a
Seba Luna <ramon.sebastian.luna@gmail.com>
2024-03-25 13:33:42 -0300

    chore: initial commit

---
+++ agregar.php
@@ -0,0 +1,10 @@
+<?php
+
+//TOMAR LOS DATOS QUE ENVIASTE DESDE index.html
+
+//Recorrer el archivo usuarios.csv
+
+//Si el DNI existe en usuarios.csv devolver mensaje a JS
+
+//Si el DNI NO existe en usuarios.csv agregarlo, con el apelillido, separado por ";" 
+// y devolver respuesta a JS
